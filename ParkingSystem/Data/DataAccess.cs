d3a177b499ca83ce3ebc80ff72d90d77d4045682
Ceco ceco <tsvetomirpch78@gmail.com>
2022-04-01 13:12:40 +0300

    Added new Class, list of cars

---
+++ ParkingSystem/Data/DataAccess.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ParkingSystem.Data
+{
+    public class DataAccess
+    {
+        public static List<Car> Cars { get; set; } = new List<Car>();
+    }
+}
--- ParkingSystem/Data/Models/Car.cs
+++ ParkingSystem/Data/Models/Car.cs
@@ -1,6 +1,13 @@
-﻿namespace ParkingSystem.Data.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingSystem.Data.Models
 {
     public class Car
     {
+        [Required]
+        public string CarMake { get; set; }
+        [Required]
+        public string PlateNumber { get; set; }
+
     }
 }
