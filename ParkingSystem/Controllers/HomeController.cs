f087a5ce32437143ac7211fb88e21ef2ed08540f
Ceco ceco <tsvetomirpch78@gmail.com>
2022-04-02 17:29:41 +0300

    Added Car Controller

--- ParkingSystem/Controllers/HomeController.cs
+++ ParkingSystem/Controllers/HomeController.cs
@@ -12,21 +12,6 @@ namespace ParkingSystem.Controllers
         public IActionResult Index()
         {
             return View(DataAccess.Cars);
-        }
-
-        [HttpPost]
-        public IActionResult AddCar(Car car)
-        {
-            DataAccess.Cars.Add(car);
-            return Redirect("/");
-        }
-
-        [HttpPost]
-        public IActionResult DeleteCar(string plateNumber)
-        {
-            var car = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);
-            DataAccess.Cars.Remove(car);
-            return Redirect("/");
-        }
+        }       
     }
 }
